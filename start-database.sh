0598853812f55faaa0263dc3595a89e86d821fe3
Patrick Frenett <patrick@frenett.net>
2024-05-29 19:44:01 -0400

    fix: fix ci

--- start-database.sh
+++ start-database.sh
@@ -27,9 +27,9 @@ if [ "$(docker ps -q -a -f name=$DB_CONTAINER_NAME)" ]; then
     exit 0
 fi
 
-# import env variables from .env
+# import env variables from .env.local
 set -a
-source .env
+source .env.local
 
 DB_PASSWORD=$(echo "$DATABASE_URL" | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
 
@@ -37,12 +37,12 @@ if [ "$DB_PASSWORD" = "password" ]; then
     echo "You are using the default database password"
     read -p "Should we generate a random password for you? [y/N]: " -r REPLY
     if ! [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo "Please set a password in the .env file and try again"
+        echo "Please set a password in the .env.local file and try again"
         exit 1
     fi
     # Generate a random URL-safe password
     DB_PASSWORD=$(openssl rand -base64 12 | tr '+/' '-_')
-    sed -i -e "s#:password@#:$DB_PASSWORD@#" .env
+    sed -i -e "s#:password@#:$DB_PASSWORD@#" .env.local
 fi
 
 docker run -d \
